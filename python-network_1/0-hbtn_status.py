b351c9ff7c362ac244b78832cf42628fcfdcf9d4
mnelson-1 <m.nelson@alustudent.com>
2023-11-30 13:35:17 -0800

    added

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     request = urllib.request.Request("https://intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
+        print("Body response: ")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
         print("\t- utf8 content: {}".format(body.decode("utf-8")))

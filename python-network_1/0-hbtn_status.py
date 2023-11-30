a579c4496b7f78f5de86041c69f7487bd4539f88
mnelson-1 <m.nelson@alustudent.com>
2023-11-30 13:15:27 -0800

    netwok 1 completed

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status."""
+import urllib.request
+
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    wit
